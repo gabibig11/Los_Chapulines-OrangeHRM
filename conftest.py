62ca441cb51c1a32dc46f842f83a615ce5befb0e
Gabriela Balderrama Guevara <gabriela.balderrama.guevara@gmail.com>
2024-06-27 18:15:28 -0400

    Git ignore y Login hecho por Sebastisn

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+
+system_url= "https://api-sandbox.orangehrm.com"
+@pytest.fixture
+def test_login():
+    url = f'{system_url}/oauth/issueToken'
+    #url = 'https://api-sandbox.orangehrm.com/oauth/issueToken'
+    payload = {'client_id': 'api-client', 'client_secret': '942d36a36d6bf422a36f5871f905b6e5',
+               'grant_type': 'client_credentials'}
+    response = requests.post(url, data=payload)
+    response_data = response.json()
+    access_token = response_data["access_token"]
+    assert response.status_code == 200
+    assert response_data["access_token"] is not None
+    return access_token
