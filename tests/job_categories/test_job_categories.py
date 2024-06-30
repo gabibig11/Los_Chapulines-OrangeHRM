c226509f5a57a7ef4b69aa1ddc66ae30554f4f2d
SusanDanielaE <201604520@est.umss.edu>
2024-06-30 17:42:24 -0400

    H202: Se agrego el test para verificar el schema de jobCategories

---
+++ tests/job_categories/test_job_categories.py
@@ -0,0 +1,15 @@
+import requests
+from tests.config import system_url
+from src.orangeHRM_api.endpoints import Endpoints
+from src.utils.load_resources import load_schema_resource
+import jsonschema
+
+
+def test_job_categories_schema_file(test_login):
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+
+    schema = load_schema_resource("job_categories_schema.json")
+    assert jsonschema.validate(instance=response_data, schema=schema) is None
