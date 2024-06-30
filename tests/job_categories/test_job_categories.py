3ae3653ac276f9ef435f20bcaa9cdcc7e6eb8e5b
SusanDanielaE <201604520@est.umss.edu>
2024-06-30 17:43:14 -0400

    H202: Se agrego el test para verificar que job categories retorne los datos

--- tests/job_categories/test_job_categories.py
+++ tests/job_categories/test_job_categories.py
@@ -13,3 +13,12 @@ def test_job_categories_schema_file(test_login):
 
     schema = load_schema_resource("job_categories_schema.json")
     assert jsonschema.validate(instance=response_data, schema=schema) is None
+
+
+def test_job_categories_success(test_login):
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    assert response.status_code == 200
+    assert response_data['data'] is not None
