43b36ae36332aacce4ef09ae5997f16cee9396f2
SusanDanielaE <201604520@est.umss.edu>
2024-07-06 20:44:51 -0400

    H201: se agregaron test para job cartegories post

--- tests/job_categories/test_job_categories_post.py
+++ tests/job_categories/test_job_categories_post.py
@@ -4,8 +4,9 @@ from src.assertions.job_categories_assertions import assert_job_categories_creat
 import json
 from src.orangeHRM_api.api_requests import OrangeRequests
 from conftest import post_teardown
+import pytest
 
-def test_job_categories_success(test_login):
+def test_job_categories_add_success(test_login):
     job_category_name = "Prueba Orange3"
     data = {
         "name" : job_category_name
@@ -19,7 +20,67 @@ def test_job_categories_success(test_login):
     response_data = response_json["data"]
 
     assert_job_categories_created(response)
-    # assert_add_job_categories_schema(response)
-    assert response_data["name"] == job_category_name
+    assert_add_job_categories_schema(response)
     post_teardown(url, headers, response, "data")
-    
+
+
+def test_job_categories_add_name_already_exists(test_login):
+    job_category_name = "Prueba Orange2"
+    data = {
+        "name" : job_category_name
+    }
+
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = OrangeRequests().post(url, headers=headers, data=data)
+    assert response.status_code == 400
+
+
+def test_job_categories_add_name_than_50_characters(test_login):
+    job_category_name = "Este es un nombre muuuuuuuuuuuuuuuuuuuuuuy laaargoooooooo"
+    data = {
+        "name" : job_category_name
+    }
+
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = OrangeRequests().post(url, headers=headers, data=data)
+    assert response.status_code == 500
+
+
+@pytest.mark.xfail(reason="Crea una categoria de trabajo con solo dos caracteres-H202 Verificar que no se pueda agregar una categoría de trabajo con menos de dos caracteres en el name.")
+def test_job_categories_add_name_less_than_2_characters(test_login):
+    job_category_name = "as"
+    data = {
+        "name" : job_category_name
+    }
+
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = OrangeRequests().post(url, headers=headers, data=data)
+    assert response.status_code == 500
+
+
+def test_job_categories_add_empty_name(test_login):
+    job_category_name = " "
+    data = {
+        "name" : job_category_name
+    }
+
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = OrangeRequests().post(url, headers=headers, data=data)
+    assert response.status_code == 500
+
+
+@pytest.mark.xfail(reason="Crea una categoria de trabajo con solo caracteres especiales-H202 Verificar que no se pueda agregar una categoría de trabajo con solo caracteres especiales en name")
+def test_job_categories_add_name_only_special_characters(test_login):
+    job_category_name = "++++"
+    data = {
+        "name" : job_category_name
+    }
+
+    url = f'{system_url}{Endpoints.job_categories.value}'
+    headers = {'Authorization': f'{test_login}'}
+    response = OrangeRequests().post(url, headers=headers, data=data)
+    assert response.status_code == 500
