82d9e601924c86195c5f6f69a5f817559122269b
danielairi <49346866+danielairi@users.noreply.github.com>
2024-07-08 16:17:56 -0300

    Cambios

---
+++ tests/employment_status/test_employment_status_post.py
@@ -0,0 +1,98 @@
+import json
+import pytest
+from conftest import *
+from src.assertions.employment_status_assertions import *
+from src.orangeHRM_api.endpoints import Endpoints
+
+#1Verificar que se pueda agregar un status laboral
+@pytest.mark.smoke
+def test_employment_status_add(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+            "name": "LuquitasMosqueraGutierrez"
+        }
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 201
+
+
+
+#2Verificar que no se pueda agregar un status que ya esta agregado
+def test_employment_status_already_added(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": "MauricioChaconLujan"
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 400
+
+#3Verificar que no se pueda agregar un status laboral con más de 60 caracteres
+
+@pytest.mark.xfail(reason="agrega un status con mas de 60 caracteres")
+def test_employment_60_characters(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 500
+
+#4Verificar que no se pueda agregar un estatus laboral de cero caracteres
+@pytest.mark.xfail(reason="agrega un status con 0 caracteres")
+def test_employment_0_characters(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": " "
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 500
+
+#5Verificar que no se pueda agregar un status laboral de un carácter
+@pytest.mark.xfail(reason="agrega un status con 1 caracteres")
+def test_employment_1_characters(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": "a"
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 500
+
+#6Verificar que no se acepte un numero al agregar un status laboral
+@pytest.mark.xfail(reason="agrega un status con caracteres numericos")
+def test_employment_1_characters(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": "123"
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 500
+#7Verificar que no se acepte caracteres especiales al agregar un status laboral
+
+@pytest.mark.xfail(reason="agrega un status con caracteres especiales")
+def test_employment_special_characters(test_login):
+    url = f'{system_url}{Endpoints.employment_status.value}'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{test_login}'}
+    payload = {
+
+        "name": "@"
+    }
+
+    response = OrangeRequests().post(url=url, headers=headers, data=payload)
+    assert response.status_code == 500
+
