5b719a235fc10c36457fabdc386415e21ec6ca17
selena santa cruz <selenasantacruzcrespo@gmail.com>
2024-07-09 06:52:07 -0400

    Smoke Test 2

--- src/orangeHRM_api/endpoints.py
+++ src/orangeHRM_api/endpoints.py
@@ -2,18 +2,17 @@ from enum import Enum
 
 
 class Endpoints(Enum):
-    delete_nationalit = None
     login = "/oauth/issueToken"
     job_titles = "/api/jobTitles"
     nationality_list = "/api/nationality"
-    getusers_id = "/api/systemUsers/"
-    getusers_filter = "/api/systemUsers"
+    getusers_id="/api/systemUsers/"
+    getusers_filter="/api/systemUsers"
     job_categories = "/api/jobCategories"
     location = "/api/locations"
     employment_status = "/api/employmentStatus"
     subunits = "/api/subunits"
 
-    postvacancy = '/api/recruitment/vacancies'
-    postcustormer = '/api/customers'
-    patchusers = '/api/systemUsers/'
-    deletenationality = '/api/nationality?='
+    postvacancy='/api/recruitment/vacancies'
+    postcustormer='/api/customers'
+    patchusers='/api/systemUsers/'
+    getcustomer='/api/customers/'
