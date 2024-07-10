7d39ce63e3e890bf5b381f0658a21a1fc16e28fd
Ethan <goitiaethan@gmail.com>
2024-07-10 10:46:45 -0400

    H302 Correccion de archivo post

--- tests/job_title/test_job_title_post.py
+++ tests/job_title/test_job_title_post.py
@@ -7,7 +7,8 @@ from src.assertions.job_title_assertions import assert_job_title_post_schema, as
 
 # Tomar en cuenta que el único label obligatorio es jobTitleName para crear un jobTitle
 
-# Verificar que se pueda añadir un cargo laboral con todos los parámetros correctos. (**SMOKE**)
+
+# Verificar que se pueda añadir un cargo laboral con todos los parámetros correctos.
 @pytest.mark.smoke
 def test_job_title_post_success(test_login):
     url = f'{system_url}{Endpoints.job_titles.value}'
